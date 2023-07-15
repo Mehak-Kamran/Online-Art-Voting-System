1fc2cd89dc86a8fc6db3e7d96de1b32add061beb
Mehak Kamran <130895685+Mehak-Kamran@users.noreply.github.com>
2023-07-15 20:56:59 +0500

    Add files via upload
    Routes and CSS files

--- register.php
+++ register.php
@@ -1,33 +1,45 @@
-<?php
-    include("connection.php");
+<html>
+    <head>
+        <title>Online voting system - Registratrion</title>
+        <link rel="stylesheet" href="../css/stylesheet.css">
+    </head>
+    <body id="regbody">
+        <center>
+            
+            <div id="regform">
+            <h2 style="color:#13a796;">Registration</h2>
+            <form action="../api/register.php" method="POST" enctype="multipart/form-data">
+    <input type="text" name="name" placeholder="Name" required>&nbsp;
+    <input type="number" name="mob" placeholder="Mobile" required><br><br>
+    <input type="password" name="pass" placeholder="Password" required>&nbsp;
+    <input type="password" name="cpass" placeholder="Confirm Password" required><br><br>
+    <input style="width: 42%" type="text" name="add" placeholder="Address" required><br><br>
 
-    $name = $_POST['name'];
-    $mobile = $_POST['mob'];
-    $pass = $_POST['pass'];
-    $cpass = $_POST['cpass'];
-    $add = $_POST['add'];
-    $image = $_FILES['image']['name'];
-    $tmp_name = $_FILES['image']['tmp_name'];
-    $role = $_POST['role'];
+    <div id="upload" style="width: 80%">
+        <input type="file" id="file" name="image">
+        <label id="label" for="file">Upload your Art</label>
+    </div><br>
 
-    if($cpass!=$pass){
-        echo '<script>
-                alert("Passwords do not match!");
-                window.location = "../routes/register.php";
-            </script>';
-    }
-    else{
-        move_uploaded_file($tmp_name,"../uploads/$image");
-    
-        $insert = mysqli_query($connect, "insert into user (name, mobile, password, address, photo, status, votes, role) values('$name', '$mobile', '$pass', '$add', '$image', 0, 0, '$role') ");
-        $insert = mysqli_query($connect, "insert into login ( mobile, password,role) values('$mobile', '$pass','$role') ");
-        
-        if($insert){
-            echo '<script>
-                    alert("Registration successfull!");
-                    window.location = "../";
-                </script>';
+    <div id="upload" style="width: 25%">
+        <select name="role" id="role" onchange="toggleRequired()">
+            <option value="1">Voter</option>
+            <option value="2">Artist</option>
+        </select><br>
+    </div><br>
+
+    <button id="reg" type="submit" name="registerbtn">Register</button><br><br>
+    Already a user? <a href="../">Login here</a>
+</form>
+
+<script>
+    function toggleRequired() {
+        var roleSelect = document.getElementById('role');
+        var fileInput = document.getElementById('file');
+
+        if (roleSelect.value === '2') {
+            fileInput.setAttribute('required', 'required');
+        } else {
+            fileInput.removeAttribute('required');
         }
     }
-    
-?>
+</script>
